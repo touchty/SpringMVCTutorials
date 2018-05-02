15145f78cd50ae315f9a85504f36e587a125e7f3
touchty <touchxyz@163.com>
2018-05-02 23:55:46 +0800

    idea

--- src/com/tutorialspoint/HelloController.java
+++ src/com/tutorialspoint/HelloController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.ui.ModelMap;
 
 @Controller
+@RequestMapping("/hello")
 public class HelloController {
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String printHello(ModelMap model) {
+    @RequestMapping(method = RequestMethod.GET)public String printHello(ModelMap model) {
         model.addAttribute("message", "Hello Spring MVC Framework!");
         return "hello";
     }
